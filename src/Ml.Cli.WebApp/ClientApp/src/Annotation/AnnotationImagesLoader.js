8c1eb0b8dcb1bdf5577ba7bb9ec68d0c25985ca2
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-07-05 16:24:52 +0200

    feat(Annotation): import Ocr and Toolbar from Ml-Toolkit

--- src/Ml.Cli.WebApp/ClientApp/src/Annotation/AnnotationImagesLoader.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Annotation/AnnotationImagesLoader.js
@@ -1,6 +1,8 @@
 ﻿import React, {useEffect, useState} from "react";
 import EditorContainer from "../Editor/EditorContainer";
 import {fetchGetData} from "../FetchHelper";
+import OcrContainer from "./Toolkit/Ocr";
+import url from "./Toolkit/Ocr/sample_rib.png";
 
 const fetchImages = async data => {
     if (data.status === 200) {
@@ -40,13 +42,26 @@ const AnnotationImagesLoader = ({item, expectedOutput, onSubmit, MonacoEditor, f
         getUrls();
     }, []);
 
+    const labels =   [{name: "Recto", color: "#212121", id: 0}, {name: "Verso", color: "#ffbb00", id: 1}, {name: "Signature", color: "#f20713", id: 2}];
+    const onOcrSubmit = (e) => {
+        console.log("Submit Method", e);
+    };
+    
     return (
-        <EditorContainer
-            expectedOutput={expectedOutput}
-            urls={state.fileUrls}
-            onSubmit={onSubmit}
-            MonacoEditor={MonacoEditor}
-        />
+        <>
+            <EditorContainer
+                expectedOutput={expectedOutput}
+                urls={state.fileUrls}
+                onSubmit={onSubmit}
+                MonacoEditor={MonacoEditor}
+            />
+            <OcrContainer
+                labels={labels}
+                expectedLabels={[]}
+                url={url}
+                onSubmit={onOcrSubmit}
+            />
+        </>
     );
 };
 
