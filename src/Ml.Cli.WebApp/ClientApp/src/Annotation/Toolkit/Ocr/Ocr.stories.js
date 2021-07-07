6c604e1177fb853a5114a97a2d1fbb1baf411110
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-07-07 10:45:21 +0200

    refactor(Storybook): update ocr story labels and add default image

--- src/Ml.Cli.WebApp/ClientApp/src/Annotation/Toolkit/Ocr/Ocr.stories.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Annotation/Toolkit/Ocr/Ocr.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import OcrContainer from "./index";
 import url from "./sample_rib.png";
 
-const labels =   [{name: "Recto", color: "#212121", id: 0}, {name: "Verso", color: "#ffbb00", id: 1}, {name: "Signature", color: "#f20713", id: 2}];
+const labels =   [{name: "Eyes color", id: 0}, {name: "Description", id: 1}];
 
 const onSubmit = (e) => {
   console.log("Submit Method", e);
