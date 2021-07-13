dca7e760ee7064846ee0632aa09deeb68508a6ad
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-07-13 15:33:44 +0200

    refactor(Rotation): add lazy module loader component

---
+++ src/Ml.Cli.WebApp/ClientApp/src/Annotation/Toolkit/Rotation/IrotLazy.js
@@ -0,0 +1,17 @@
+﻿import React, {Suspense} from "react";
+import IrotContainer from "./Irot.container";
+
+const IrotLazy = ({expectedLabels, url, onSubmit}) => {
+    
+    const IrotContainer = React.lazy(() => import('./Irot.container'));
+    
+    return <Suspense fallback={<div>Chargement de la rotation...</div>}>
+        <IrotContainer
+            expectedLabels={expectedLabels}
+            url={url}
+            onSubmit={onSubmit}
+        />
+    </Suspense>
+};
+
+export default IrotLazy;
