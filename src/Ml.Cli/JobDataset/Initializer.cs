f5201f7be45b6db6513fb83070d4d0f3562f0436
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-07-20 09:19:15 +0200

    refactor(Script): add ScriptManager static class and make "script" parameter of dataset task non mandatory

--- src/Ml.Cli/JobDataset/Initializer.cs
+++ src/Ml.Cli/JobDataset/Initializer.cs
@@ -1,22 +1,30 @@
-﻿using Ml.Cli.Helper;
+﻿using System;
+using Ml.Cli.Helper;
 using Newtonsoft.Json.Linq;
 
 namespace Ml.Cli.JobDataset
 {
     public class Initializer
     {
-        public static DatasetTask CreateTask(JObject jObject, string type, bool enabled, bool isBaseDirectoryValid, string baseDirectory, string taskId, IPathValidatorHelper pathValidatorHelper)
+        public static DatasetTask CreateTask(JObject jObject, string type, bool enabled, bool isBaseDirectoryValid,
+            string baseDirectory, string taskId, IPathValidatorHelper pathValidatorHelper)
         {
+            var isScriptDefined = jObject.Property("script", StringComparison.Ordinal) != null;
+            var script = isScriptDefined ? (string) jObject.Property("script") : "";
             return new DatasetTask(
                 taskId,
                 type,
                 enabled,
                 (string) jObject.Property("annotationType"),
                 (string) jObject.Property("configuration"),
-                PropertyHelper.SetProperty(jObject, "fileDirectory", isBaseDirectoryValid, baseDirectory, taskId, pathValidatorHelper),
-                PropertyHelper.SetProperty(jObject, "imageDirectory", isBaseDirectoryValid, baseDirectory, taskId, pathValidatorHelper),
-                PropertyHelper.SetProperty(jObject, "outputDirectory", isBaseDirectoryValid, baseDirectory, taskId, pathValidatorHelper),
-                (string) jObject.Property("fileName")
+                PropertyHelper.SetProperty(jObject, "fileDirectory", isBaseDirectoryValid, baseDirectory, taskId,
+                    pathValidatorHelper),
+                PropertyHelper.SetProperty(jObject, "imageDirectory", isBaseDirectoryValid, baseDirectory, taskId,
+                    pathValidatorHelper),
+                PropertyHelper.SetProperty(jObject, "outputDirectory", isBaseDirectoryValid, baseDirectory, taskId,
+                    pathValidatorHelper),
+                (string) jObject.Property("fileName"),
+                script
             );
         }
     }
---
+++ src/Ml.Cli/JobScript/ScriptManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ClearScript.V8;
+using Ml.Cli.FileLoader;
+using Newtonsoft.Json.Linq;
+
+namespace Ml.Cli.JobScript
+{
+    public static class ScriptManager
+    {
+        // ReSharper disable once MemberCanBePrivate.Global : Class used by the V8ScriptEngine, must be public
+        public static class EngineConsole
+        {
+            // ReSharper disable once InconsistentNaming : JavaScript naming convention
+            // ReSharper disable once UnusedMember.Global
+            public static void log(string value) => Console.WriteLine(value);
+        }
+        
+        public static async Task<string> ExecScript(string path, string script, IFileLoader fileLoader)
+        {
+            var engine = new V8ScriptEngine();
+            engine.AddHostType("console", typeof(EngineConsole));
+            var json = JToken.Parse(await fileLoader.LoadAsync(path));
+            var jsonResult = (JObject) json;
+            var body = (string) jsonResult.Property("Body");
+            engine.Script.rawBodyInput = body;
+            engine.Execute(script);
+            return (string) engine.Script.rawBodyOutput;
+        }
+    }
+}
