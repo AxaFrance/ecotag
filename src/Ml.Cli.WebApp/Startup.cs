c708811134a5009594b9af57aa468722d67e8ac0
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-07-29 15:30:31 +0200

    refactor(WebApp): use https redirection only if not in development environment

--- src/Ml.Cli.WebApp/Startup.cs
+++ src/Ml.Cli.WebApp/Startup.cs
@@ -39,9 +39,9 @@ namespace Ml.Cli.WebApp
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
