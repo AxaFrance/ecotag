1145c1f155326104d1adb38782b924af164b66af
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-08-12 14:12:15 +0200

    feat(WebApp): add help command and arguments description and shortcuts Make securityPath an optional argument

--- src/Ml.Cli.WebApp/Program.cs
+++ src/Ml.Cli.WebApp/Program.cs
@@ -1,7 +1,9 @@
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.CommandLineUtils;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ml.Cli.WebApp.BasePath;
@@ -12,12 +14,58 @@ namespace Ml.Cli.WebApp
     {
         public static async Task Main(string[] args)
         {
-            if (!args[0].EndsWith(Path.DirectorySeparatorChar))
+            var app = new CommandLineApplication()
             {
-                args[0] += Path.DirectorySeparatorChar;
+                Name = "Ml-Cli.exe",
+                FullName = "Ml-Cli",
+                Description =
+                    "Ml-Cli is an open-source, local tool that automates Machine Learning actions such as quality tests on services, dataset creation and annotation."
+            };
+            app.HelpOption("-?|-h|--help");
+
+            var tasksPath = app.Option("-t|--tasks-path",
+                "[Required] - The path of the tasks.json, the file that describes the tasks to execute.",
+                CommandOptionType.SingleValue);
+            var basePath = app.Option("-b|--base-path",
+                "[Required] - Default base directory used by the path inside your task.json.", CommandOptionType.SingleValue);
+            var securityPath = app.Option("-s|--security-path",
+                "[Optional] - Security directory path. ml-cli has only access to files inside this directory. If not provided, the security path will be the same as the base path.",
+                CommandOptionType.SingleValue);
+
+            app.OnExecute(async() =>
+            {
+                var tasksValue = tasksPath.Value();
+                var baseValue = basePath.Value();
+                if (tasksValue == null || baseValue == null)
+                {
+                    app.ShowHelp();
+                    return -1;
+                }
+                var securityValue = securityPath.Value() == null ? baseValue : securityPath.Value();
+                if (!securityValue.EndsWith(Path.DirectorySeparatorChar))
+                {
+                    securityValue += Path.DirectorySeparatorChar;
+                }
+
+                var providedArgs = new[]
+                {
+                    securityValue,
+                    tasksValue,
+                    baseValue
+                };
+                var builder = CreateHostBuilder(providedArgs).Build();
+                await builder.RunAsync();
+                return 0;
+            });
+            try
+            {
+                app.Execute(args);
             }
-            var builder = CreateHostBuilder(args).Build();
-            await builder.RunAsync();
+            catch (CommandParsingException ex)
+            {
+                Console.WriteLine(ex.Message);
+                app.ShowHelp();
+            }
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -26,7 +74,7 @@ namespace Ml.Cli.WebApp
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddSingleton<IHostedService>(provider =>
-                    new Worker(args.Skip(1).ToArray()));
+                        new Worker(args.Skip(1).ToArray()));
                     services.AddSingleton<IBasePath, BasePath.BasePath>(provider => new BasePath.BasePath(args[0]));
                 });
     }
