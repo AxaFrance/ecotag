f83d6caad4d4ecc3c77073e7f592721870d3901f
lilian-delouvy <58565718+lilian-delouvy@users.noreply.github.com>
2021-08-20 12:06:36 +0200

    feat(TitleBar): add TitleBar component (#72)

--- src/Ml.Cli.WebApp/ClientApp/src/Annotation/Annotate.spec.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Annotation/Annotate.spec.js
@@ -2,6 +2,7 @@
 import {fireEvent, render, waitFor} from "@testing-library/react";
 import Annotate from "./Annotate";
 import sleep from "../sleep";
+import {BrowserRouter} from "react-router-dom";
 
 const dataset = (`{"DatasetLocation": "C:\\\\location", "AnnotationType": "JsonEditor", "Configuration": \"[{\\\"name\\\": \\\"Recto\\\", \\\"id\\\": 0}, {\\\"name\\\": \\\"Verso\\\", \\\"id\\\": 1}]\", "Content": [{\"FileName\": \"{FileName}_pdf.json\",\"FileDirectory\": \"fileDirectoryValue\",\"ImageDirectory\": \"imageDirectoryValue\",\"Annotations\":{}}]}`);
 
@@ -20,7 +21,7 @@ const fetch = (status =200) => async (url, config) => {
 describe("Check dataset handling", () => {
     test("Should insert dataset and select ocr and rotation annotation types", async () => {
         const mockedMonacoEditor = () => (<div>This is a mocked Monaco Editor</div>);
-        const {container, asFragment, getAllByText, getByAltText} = render(<Annotate MonacoEditor={mockedMonacoEditor} fetchFunction={fetch(200)}/>);
+        const {container, asFragment, getAllByText, getByAltText} = render(<BrowserRouter><Annotate MonacoEditor={mockedMonacoEditor} fetchFunction={fetch(200)}/></BrowserRouter>);
 
         const blob = new Blob([dataset], {type: "text/plain;charset=utf-8"});
         const fileSource = new File([blob], "dataSourceFile.json", {type: "application/json"});
