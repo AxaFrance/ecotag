ab61be5005ef4862be374dd70577778289340f03
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-08-27 15:24:44 +0200

    test(FilesController): add files controller unit test

--- tests/Ml.Cli.WebApp.Tests/FilesControllerUnitTest.cs
+++ tests/Ml.Cli.WebApp.Tests/FilesControllerUnitTest.cs
@@ -1,7 +1,33 @@
-﻿namespace Ml.Cli.WebApp.Tests
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32.SafeHandles;
+using Ml.Cli.FileLoader;
+using Ml.Cli.WebApp.Controllers;
+using Ml.Cli.WebApp.Paths;
+using Moq;
+using Xunit;
+
+namespace Ml.Cli.WebApp.Tests
 {
     public class FilesControllerUnitTest
     {
-        
+        [Fact]
+        public async Task ShouldReturnFile()
+        {
+            var fileValue = "value=C:\\someFolderWith%2BSign\\{fileName}.png";
+            
+            var basePath = new Mock<BasePath>("");
+            basePath.Setup(mock => mock.IsPathSecure(It.IsAny<string>())).Returns(true);
+            
+            var fileLoader = new Mock<IFileLoader>();
+            fileLoader.Setup(mock => mock.OpenRead("C:\\someFolderWith+Sign\\{fileName}.png"))
+                .Returns(new MemoryStream());
+            
+            var filesController = new FilesController(fileLoader.Object, basePath.Object);
+
+            var result = await filesController.ShowFile(fileValue) as FileStreamResult;
+            Assert.Equal("image/png", result.ContentType);
+        }
     }
 }
