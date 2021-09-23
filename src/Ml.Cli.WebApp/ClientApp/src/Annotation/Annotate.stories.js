be67636f5e1b4dcbcdf87346e94593d53298da47
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-09-23 14:19:43 +0200

    refactor(Annotate): update Annotate component storybook

--- src/Ml.Cli.WebApp/ClientApp/src/Annotation/Annotate.stories.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Annotation/Annotate.stories.js
@@ -1,18 +1,21 @@
 ﻿import React from "react";
 import Annotate from "./Annotate";
+import {HashRouter} from "react-router-dom";
 
 export default {
     title: 'Annotation',
     component: Annotate
 };
 
-const Template = (args) => <Annotate {...args} />;
+const MonacoEditor = React.lazy(() => import("@monaco-editor/react"));
+const mockedFetch = async (queryUrl, data) => Promise.resolve({ok: false, status: 400, statusText: "Bad Request", bodyUsed: false});
+
+const Template = (args) => <HashRouter basename={window.location.pathname}><Annotate {...args} /></HashRouter>;
 
 export const Default = Template.bind({});
 Default.args = {
-    state: {
-        fileName: "Annoter un dataset"
-    }
+    MonacoEditor: MonacoEditor,
+    fetchFunction: mockedFetch
 };
 
 Default.parameters = {
