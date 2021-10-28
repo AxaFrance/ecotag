c2bc2c2b9a3a0280a8dedf401b14e549c027e80e
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-10-28 16:42:46 +0200

    test(Annotate): update unit test

--- src/Ml.Cli.WebApp/ClientApp/src/Annotation/Annotate.spec.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Annotation/Annotate.spec.js
@@ -2,13 +2,12 @@
 import {fireEvent, render, waitFor} from "@testing-library/react";
 import Annotate from "./Annotate";
 import sleep from "../sleep";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, useHistory} from "react-router-dom";
 
 const dataset = (`{"DatasetLocation": "C:\\\\location", "AnnotationType": "JsonEditor", "Configuration": \"[{\\\"name\\\": \\\"Recto\\\", \\\"id\\\": 0}, {\\\"name\\\": \\\"Verso\\\", \\\"id\\\": 1}]\", "Content": [{\"FileName\": \"{FileName}_pdf.json\",\"FileDirectory\": \"fileDirectoryValue\",\"ImageDirectory\": \"imageDirectoryValue\",\"Annotations\":{}}]}`);
 
 const fetch = (status =200) => async (url, config) => {
     await sleep(1);
-    console.log(url);
     return {
         status:status,
         json: async () => {
@@ -19,7 +18,7 @@ const fetch = (status =200) => async (url, config) => {
 };
 
 describe("Check dataset handling", () => {
-    test("Should insert dataset and select ocr and rotation annotation types", async () => {
+    test("Should insert dataset", async () => {
         const mockedMonacoEditor = () => (<div>This is a mocked Monaco Editor</div>);
         const {container, asFragment, getAllByText, getByAltText} = render(<BrowserRouter><Annotate MonacoEditor={mockedMonacoEditor} fetchFunction={fetch(200)}/></BrowserRouter>);
 
@@ -27,10 +26,8 @@ describe("Check dataset handling", () => {
         const fileSource = new File([blob], "dataSourceFile.json", {type: "application/json"});
         const fileInput = container.querySelector("input[type='file']");
         fireEvent.change(fileInput, {target: {files: [fileSource]}});
-
-        await waitFor(() => expect(container.querySelector('.table-result')).not.toBeNull());
+        
         await waitFor(() => expect(getAllByText(/Visualising file: dataSourceFile.json/i)).not.toBeNull());
-        await waitFor(() => expect(getByAltText(/file_image/i)).not.toBeNull());
         expect(asFragment()).toMatchSnapshot();
     });
 });
