0226c3986116e0f5b260ab5e2930f3f225de9843
Lilian Delouvy <lilian.delouvy@axa.fr>
2022-01-07 15:39:13 +0100

    refactor(EmptyArrayManager): remove unnecessary previous component declaration

--- src/Ml.Cli.WebApp/ClientApp/src/EmptyArrayManager/index.js
+++ src/Ml.Cli.WebApp/ClientApp/src/EmptyArrayManager/index.js
@@ -1,32 +1,14 @@
-﻿import React, {Component} from "react";
+﻿import React from "react";
 import './EmptyArrayManager.scss';
 
-class EmptyArrayManager2 extends Component {
-    
-    render(){
-        const child = React.Children.only(this.props.children);
-        if('items' in child.props){
-            if(child.props.items.length){
-                return <>{child}</>
-            }
+const EmptyArrayManager = ({items, emptyArrayMessage, children}) => 
+    <>
+        {items.length ? (
+            <>{children}</>
+        ) : (
+            <span className="empty-array__error"><b>{emptyArrayMessage}</b></span>
+        )
         }
-        return <span className="empty-array__error"><b>{this.props.onEmptyArray}</b></span>
-    };
-}
-
-const EmptyArrayManager = ({items, emptyArrayMessage, children}) => {
-    
-    return(
-        <>
-            {items.length ? (
-                <>{children}</>
-            ) : (
-                <span className="empty-array__error"><b>{emptyArrayMessage}</b></span>
-            )
-            }
-        </>
-    )
-    
-}
+    </>
 
 export default EmptyArrayManager;
