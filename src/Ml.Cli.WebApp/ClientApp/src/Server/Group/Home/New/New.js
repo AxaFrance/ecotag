a3bd1d8ac76f25b3f33cf7082640269ab256f8dc
Lilian Delouvy <58565718+lilian-delouvy@users.noreply.github.com>
2022-02-02 13:55:29 +0100

    fix(Group): prevent New component button and textarea from overlapping (#165)

--- src/Ml.Cli.WebApp/ClientApp/src/Server/Group/Home/New/New.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Server/Group/Home/New/New.js
@@ -9,20 +9,20 @@ const New = ({ fields, disabled, hasSubmit, onChangeCreateGroup, onSubmitCreateG
       <article className="af-panel af-panel--new-group">
         <section className="af-panel__content af-panel__content--new-group">
           <TextInput
-            label="Nom : "
-            name={NAME}
-            id={NAME}
-            onChange={onChangeCreateGroup}
-            helpMessage="Ex : annotateurs de rib"
-            forceDisplayMessage={hasSubmit}
-            messageType="error"
-            {...fields[NAME]}
+              label="Nom:"
+              name={NAME}
+              id={NAME}
+              onChange={onChangeCreateGroup}
+              helpMessage="Ex : annotateurs de rib"
+              forceDisplayMessage={hasSubmit}
+              messageType="error"
+              {...fields[NAME]}
           />
           <Button
-            classModifier={`hasiconRight confirm ${!disabled ? 'disabled' : ''}`}
-            id="myForm"
-            onClick={onSubmitCreateGroup}
-            disabled={!disabled}>
+              classModifier={`hasiconRight confirm ${!disabled ? 'disabled' : ''}`}
+              id="myForm"
+              onClick={onSubmitCreateGroup}
+              disabled={!disabled}>
             <span className="af-btn-text">Valider</span>
             <i className="glyphicon glyphicon-arrowthin-right" />
           </Button>
