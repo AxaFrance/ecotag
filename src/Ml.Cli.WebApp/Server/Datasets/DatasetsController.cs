38d87e0089a0980f5e74dae63e4793491b5b2fb1
Lilian Delouvy <lilian.delouvy@axa.fr>
2022-02-02 15:16:15 +0100

    refactor(Controllers): remove useless imports

--- src/Ml.Cli.WebApp/Server/Datasets/DatasetsController.cs
+++ src/Ml.Cli.WebApp/Server/Datasets/DatasetsController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
-using IdentityModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
