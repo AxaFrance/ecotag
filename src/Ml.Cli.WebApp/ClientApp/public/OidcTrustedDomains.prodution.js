d3f67dc49cc6d6f9390caedecb29c4e670a509e4
guillaume chervet <guillaume.chervet@gmail.com>
2022-03-01 11:30:18 +0100

    Update

--- src/Ml.Cli.WebApp/ClientApp/public/OidcTrustedDomains.prodution.js
+++ src/Ml.Cli.WebApp/ClientApp/public/OidcTrustedDomains.prodution.js
@@ -1,3 +1,3 @@
 ﻿
 // Add here trusted domains, access tokens will be send to 
-const trustedDomains = { default:["#{Oidc:ServiceWorkerTrustedDomain}#"]};
+const trustedDomains = { default:["#{Spa:Oidc:ServiceWorkerTrustedDomain}#"]};
