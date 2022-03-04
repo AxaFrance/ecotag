9831aaa3263993a8cd06a36611057b21d9dd6e39
Lilian Delouvy <lilian.delouvy@axa.fr>
2022-03-04 16:03:02 +0100

    test(Projects): add unit test to check error on group creation

--- tests/Ml.Cli.WebApp.Tests/Server/Projects/ProjectsControllerTest.cs
+++ tests/Ml.Cli.WebApp.Tests/Server/Projects/ProjectsControllerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Ml.Cli.WebApp.Server;
 using Ml.Cli.WebApp.Server.Projects;
 using Ml.Cli.WebApp.Server.Projects.Cmd;
 using Ml.Cli.WebApp.Server.Projects.Database;
@@ -26,27 +27,57 @@ public class ProjectsControllerTest
         projectContext.Database.EnsureCreatedAsync();
         return projectContext;
     }
-    
-    [Theory]
-    [InlineData("[]", "someProjectName")]
-    public async Task Should_Create_New_Project(string projectNamesInDatabase, string newProjectName)
-    {
-        var projectNamesArray = JsonConvert.DeserializeObject<List<string>>(projectNamesInDatabase);
-        var newProject = new CreateProjectInput { Name = newProjectName };
 
-        var projectContext = GetInMemoryProjectContext();
-        if (projectNamesArray != null)
+    public class CreateProjectTest
+    {
+        private static async Task<ProjectContext> GetProjectContext(List<string> projectNamesArray)
         {
-            projectNamesArray.ForEach(project =>
-                projectContext.Projects.Add(new ProjectModel { Name = project})
-            );
-            await projectContext.SaveChangesAsync();
+            var projectContext = GetInMemoryProjectContext();
+            if (projectNamesArray != null)
+            {
+                projectNamesArray.ForEach(project =>
+                    projectContext.Projects.Add(new ProjectModel { Name = project})
+                );
+                await projectContext.SaveChangesAsync();
+            }
+
+            return projectContext;
         }
-        var repository = new ProjectsRepository(projectContext);
-        var projectsController = new ProjectsController();
-        var createProjectCmd = new CreateProjectCmd(repository);
-        var result = await projectsController.Create(createProjectCmd, newProject);
-        var resultCreated = result.Result as CreatedResult;
-        Assert.NotNull(resultCreated);
+        
+        [Theory]
+        [InlineData("[]", "someProjectName")]
+        public async Task Should_Create_New_Project(string projectNamesInDatabase, string newProjectName)
+        {
+            var projectNamesArray = JsonConvert.DeserializeObject<List<string>>(projectNamesInDatabase);
+            var newProject = new CreateProjectInput { Name = newProjectName };
+
+            var projectContext = await GetProjectContext(projectNamesArray);
+            var repository = new ProjectsRepository(projectContext);
+            var projectsController = new ProjectsController();
+            var createProjectCmd = new CreateProjectCmd(repository);
+            var result = await projectsController.Create(createProjectCmd, newProject);
+            var resultCreated = result.Result as CreatedResult;
+            Assert.NotNull(resultCreated);
+        }
+
+        [Theory]
+        [InlineData("[\"projectName\"]", "projectName", ProjectsRepository.AlreadyTakenName)]
+        [InlineData("[]", "ab", CreateProjectCmd.InvalidModel)]
+        [InlineData("[]", "qzkjdlmqzzdizqomdqzpiduozqjidqsd", CreateProjectCmd.InvalidModel)]
+        public async Task Should_Return_Error_On_Group_Creation(string projectNamesInDatabase, string newProjectName, string errorType)
+        {
+            var projectNamesArray = JsonConvert.DeserializeObject<List<string>>(projectNamesInDatabase);
+            var newProject = new CreateProjectInput { Name = newProjectName };
+
+            var projectContext = await GetProjectContext(projectNamesArray);
+            var repository = new ProjectsRepository(projectContext);
+            var projectsController = new ProjectsController();
+            var createProjectCmd = new CreateProjectCmd(repository);
+            var result = await projectsController.Create(createProjectCmd, newProject);
+            var resultWithError = result.Result as BadRequestObjectResult;
+            Assert.NotNull(resultWithError);
+            var resultWithErrorValue = resultWithError.Value as ErrorResult;
+            Assert.Equal(errorType, resultWithErrorValue?.Key);
+        }
     }
 }
