cacea317c200ae7044501222529d741bd7580789
guillaume chervet <guillaume.chervet@gmail.com>
2022-03-07 10:16:02 +0100

    rename EcotagContent and move sql scripts

--- Scripts/Release/SQL/Azure_R1/002_create_tables.sql
+++ Scripts/Release/SQL/Azure_R1/002_create_tables.sql
@@ -28,7 +28,7 @@ END
 GO
 
 /****** Object:  Table [sch_ECOTAG].[T_GroupUsers] ******/
-if not exists (select * from sysobjects where  id = object_id(N'[sch_ECOTAG].[T_Group]') AND type in (N'U'))
+if not exists (select * from sysobjects where  id = object_id(N'[sch_ECOTAG].[T_GroupUsers]') AND type in (N'U'))
 BEGIN
 CREATE TABLE [sch_ECOTAG].[T_GroupUsers](
     [GPU_Id] uniqueidentifier NOT NULL DEFAULT newid(),
