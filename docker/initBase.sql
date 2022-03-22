0a9621bb5c08217eebac55d839dadeea1b06abc1
Lilian Delouvy <58565718+lilian-delouvy@users.noreply.github.com>
2022-03-22 09:59:58 +0100

    feat(Groups): add creatorNameIdentifier and createDate parameters to group model (#181)
    Co-authored-by: Lilian Delouvy <lilian.delouvy@axa.fr>

--- docker/initBase.sql
+++ docker/initBase.sql
@@ -87,6 +87,8 @@ BEGIN
 CREATE TABLE [sch_ECOTAG].[T_Group](
     [GRP_Id] uniqueidentifier NOT NULL DEFAULT newid(),
     [GRP_Name] [varchar](16) NOT NULL,
+    [GRP_CreatorNameIdentifier] [varchar](32) NOT NULL,
+    [GRP_CreateDate] BIGINT NOT NULL,
     CONSTRAINT [PK_T_Group] UNIQUE([GRP_Id]),
     CONSTRAINT [PK_T_Group_Name] UNIQUE([GRP_Name]),
     )
@@ -186,9 +188,9 @@ INSERT INTO [sch_ECOTAG].[T_User]([USR_Id],[USR_Email],[USR_Subject]) VALUES (@f
 INSERT INTO [sch_ECOTAG].[T_User]([USR_Id],[USR_Email],[USR_Subject]) VALUES (@secondUserId,"second@gmail.com","S222222")
 INSERT INTO [sch_ECOTAG].[T_User]([USR_Id],[USR_Email],[USR_Subject]) VALUES (@thirdUserId,"third@gmail.com","S333333")
 
-INSERT INTO [sch_ECOTAG].[T_Group]([GRP_Id],[GRP_Name]) VALUES (@firstGroupId, "firstgroup")
-INSERT INTO [sch_ECOTAG].[T_Group]([GRP_Id],[GRP_Name]) VALUES (@secondGroupId, "secondgroup")
-INSERT INTO [sch_ECOTAG].[T_Group]([GRP_Id],[GRP_Name]) VALUES (@thirdGroupId, "thirdgroup")
+INSERT INTO [sch_ECOTAG].[T_Group]([GRP_Id],[GRP_Name],[GRP_CREATORNAMEIDENTIFIER],[GRP_CREATEDATE]) VALUES (@firstGroupId, "firstgroup", "S111111", 637831187822285511)
+INSERT INTO [sch_ECOTAG].[T_Group]([GRP_Id],[GRP_Name],[GRP_CREATORNAMEIDENTIFIER],[GRP_CREATEDATE]) VALUES (@secondGroupId, "secondgroup", "S111111", 637831187625235412)
+INSERT INTO [sch_ECOTAG].[T_Group]([GRP_Id],[GRP_Name],[GRP_CREATORNAMEIDENTIFIER],[GRP_CREATEDATE]) VALUES (@thirdGroupId, "thirdgroup", "S222222", 637831187822285511)
 
 INSERT INTO [sch_ECOTAG].[T_GroupUsers]([GPU_Id],[GRP_Id],[USR_Id]) VALUES (newid(), @firstGroupId, @firstUserId)
 INSERT INTO [sch_ECOTAG].[T_GroupUsers]([GPU_Id],[GRP_Id],[USR_Id]) VALUES (newid(), @firstGroupId, @secondUserId)
--- src/Ml.Cli.WebApp/Server/Groups/Cmd/CreateGroupCmd.cs
+++ src/Ml.Cli.WebApp/Server/Groups/Cmd/CreateGroupCmd.cs
@@ -2,10 +2,11 @@
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Ml.Cli.WebApp.Server.Groups.Database.Group;
+using Ml.Cli.WebApp.Server.Groups.Database.Users;
 
 namespace Ml.Cli.WebApp.Server.Groups.Cmd;
 
-public record CreateGroupInput
+public record GroupInput
 {
     [MaxLength(16)]
     [MinLength(3)]
@@ -15,14 +16,32 @@ public record CreateGroupInput
     public List<string> UserIds { get; set; }
 }
 
+public record CreateGroupInput
+{
+    [MaxLength(16)]
+    [MinLength(3)]
+    [RegularExpression(@"^[a-zA-Z0-9-_]*$")]
+    public string Name { get; set; }
+    
+    [MaxLength(32)]
+    [MinLength(1)]
+    public string CreatorNameIdentifier { get; set; }
+    
+    public List<string> UserIds { get; set; }
+}
+
 public class CreateGroupCmd
 {
     public const string InvalidModel = "InvalidModel";
+    public const string UserNotFound = "UserNotFound";
+    
     private readonly IGroupsRepository _groupsRepository;
+    private readonly IUsersRepository _usersRepository;
 
-    public CreateGroupCmd(IGroupsRepository groupsRepository)
+    public CreateGroupCmd(IGroupsRepository groupsRepository, IUsersRepository usersRepository)
     {
         _groupsRepository = groupsRepository;
+        _usersRepository = usersRepository;
     }
 
     public async Task<ResultWithError<string, ErrorResult>> ExecuteAsync(CreateGroupInput createGroupInput)
@@ -39,8 +58,19 @@ public class CreateGroupCmd
             };
             return commandResult;
         }
+
+        var user = await _usersRepository.GetUserBySubjectAsync(createGroupInput.CreatorNameIdentifier);
+        if (user == null)
+        {
+            commandResult.Error = new ErrorResult
+            {
+                Key = UserNotFound,
+                Error = null
+            };
+            return commandResult;
+        }
         
-        var result = await _groupsRepository.CreateGroupAsync(createGroupInput.Name.ToLower());
+        var result = await _groupsRepository.CreateGroupAsync(createGroupInput);
         if (!result.IsSuccess)
         {
             commandResult.Error = result.Error;
