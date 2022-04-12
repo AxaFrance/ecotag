0e465d4db833012e866358996c8c7e30b1ad2ffd
Lilian Delouvy <lilian.delouvy@axa.fr>
2022-04-12 17:19:34 +0200

    refactor(NER): change labels text color depending on background color and highlight selected label

--- src/Ml.Cli.WebApp/ClientApp/src/Toolkit/NamedEntity/Label.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Toolkit/NamedEntity/Label.js
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 import { setLabelsColor } from './labelColor';
 import './Label.scss';
+import {adaptTextColorToBackgroundColor} from '../colors';
 
 const Label = ({labels, selectLabel, selectedLabel}) => {
     const coloredLabels = setLabelsColor(labels);
@@ -12,7 +13,7 @@ const Label = ({labels, selectLabel, selectedLabel}) => {
                     <span key={id + index} onClick={() => selectLabel(label)}>
                       <label
                           className="label__element"
-                          style={selectedLabel.id === id ? {color, backgroundColor: '#4b4848' } : {backgroundColor: color}}
+                          style={selectedLabel.id === id ? {color: adaptTextColorToBackgroundColor('#4b4848'), backgroundColor: '#4b4848', border: '4px dashed red' } : {color: adaptTextColorToBackgroundColor(color), backgroundColor: color}}
                           htmlFor={name}>
                         {name}
                       </label>
