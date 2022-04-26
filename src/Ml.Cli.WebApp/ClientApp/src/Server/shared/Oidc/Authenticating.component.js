2bb88b7ed82e871d791c16a803ad811af6368af0
Guillaume Chervet <52236059+guillaume-chervet@users.noreply.github.com>
2022-04-26 16:57:35 +0200

    feat(auth): french component and button for re-authenticate when sess… (#230)

---
+++ src/Ml.Cli.WebApp/ClientApp/src/Server/shared/Oidc/Authenticating.component.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {style} from "./style";
+
+const Authenticating = () => (
+      <div className="oidc-authenticating" style={style}>
+    <div className="oidc-authenticating__container">
+      <h1 className="oidc-authenticating__title">Authentification en cours</h1>
+      <p className="oidc-authenticating__content">Vous serez redirigé vers la page de connexion.</p>
+    </div>
+  </div>
+);
+
+export default Authenticating;
---
+++ src/Ml.Cli.WebApp/ClientApp/src/Server/shared/Oidc/Loading.component.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {style} from "./style";
+
+const Loading = () => (
+      <span className="oidc-loading" style={style}>
+    Chargement du module d'authentification
+  </span>
+);
+
+export default Loading;
