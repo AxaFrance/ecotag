a4ee027fed8c93686be9194e9ab57e788e3b3f1e
guillaume chervet <guillaume.chervet@gmail.com>
2022-04-26 16:40:36 +0200

    add unittest

--- src/Ml.Cli.WebApp/ClientApp/src/Server/shared/Oidc/ServiceWorkerNotSupported.component.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Server/shared/Oidc/ServiceWorkerNotSupported.component.js
@@ -5,7 +5,7 @@ const ServiceWorkerNotSupported = () => (
   <div className="oidc-serviceworker" style={style}>
     <div className="oidc-serviceworker__container">
       <h1 className="oidc-serviceworker__title">Impossible de s'authentifier sur ce navigateur</h1>
-      <p className="oidc-serviceworker__content">Votre browser is not secure enough to make authentication work. Try updating your browser or use a newer browser.</p>
+      <p className="oidc-serviceworker__content">Votre navigateur n’est pas assez sécurisé pour que l’authentification fonctionne. Essayez de mettre à jour votre navigateur ou d’utiliser un navigateur plus récent.</p>
     </div>
   </div>
 );
