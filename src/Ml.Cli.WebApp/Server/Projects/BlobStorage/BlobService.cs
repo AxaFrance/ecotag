8022ba3644a6c488d95d46cb96622f27b5ec3065
Lilian Delouvy <lilian.delouvy@axa.fr>
2022-04-27 10:42:55 +0200

    refactor(BlobService): wrap FileService class

--- src/Ml.Cli.WebApp/Server/Projects/BlobStorage/BlobService.cs
+++ src/Ml.Cli.WebApp/Server/Projects/BlobStorage/BlobService.cs
@@ -1,42 +1,22 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading.Tasks;
-using Azure.Storage.Blobs;
-using Microsoft.Extensions.Options;
+using Ml.Cli.WebApp.Server.Datasets.Database.FileStorage;
 
 namespace Ml.Cli.WebApp.Server.Projects.BlobStorage;
 
 [ExcludeFromCodeCoverage]
 public class BlobService : IBlobService
 {
-    private readonly IOptions<TransferFileStorageSettings> _azureBlobStorageOptions;
+    private readonly IFileService _fileService;
 
-    public BlobService(IOptions<TransferFileStorageSettings> azureBlobStorageOptions)
+    public BlobService(IFileService fileService)
     {
-        _azureBlobStorageOptions = azureBlobStorageOptions;
+        _fileService = fileService;
     }
 
     public async Task UploadStreamAsync(string containerName, string fileName, Stream fileStream)
     {
-        fileStream.Position = 0;
-        var cloudBlob = await CloudBlockBlob(containerName, fileName);
-        await cloudBlob.UploadAsync(fileStream);
-    }
-
-    private async Task<BlobContainerClient> CloudBlobContainer(string containerName)
-    {
-        var connectionString = _azureBlobStorageOptions.Value.ConnectionString;
-        var container = new BlobContainerClient(connectionString, containerName);
-        await container.CreateIfNotExistsAsync();
-        await container.SetAccessPolicyAsync();
-        return container;
-    }
-
-    private async Task<BlobClient> CloudBlockBlob(string containerName, string fileName)
-    {
-        if (string.IsNullOrEmpty(fileName)) return null;
-        var container = await CloudBlobContainer(containerName);
-        var blockBlob = container.GetBlobClient(fileName);
-        return blockBlob;
+        await _fileService.UploadStreamAsync(containerName, fileName, fileStream);
     }
 }
