ebdccbeb4ea393b3d17a41a295045f83e2681d6b
Lilian Delouvy <lilian.delouvy@axa.fr>
2022-04-29 09:25:18 +0200

    fix(TransferService): use dedicated transfer service connection string

--- src/Ml.Cli.WebApp/Server/Projects/BlobStorage/TransferService.cs
+++ src/Ml.Cli.WebApp/Server/Projects/BlobStorage/TransferService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using Ml.Cli.WebApp.Server.Datasets.Database.FileStorage;
 
 namespace Ml.Cli.WebApp.Server.Projects.BlobStorage;
@@ -10,9 +11,16 @@ public class TransferService : ITransferService
 {
     private readonly IFileService _fileService;
 
-    public TransferService(IFileService fileService)
+    public TransferService(IOptions<TransferFileStorageSettings> azureStorageOptions)
     {
-        _fileService = fileService;
+        _fileService = new FileService(
+            new OptionsWrapper<StorageSettings>(
+                    new StorageSettings
+                    {
+                        ConnectionString = azureStorageOptions.Value.ConnectionString
+                    }
+                )
+            );
     }
 
     public async Task UploadStreamAsync(string containerName, string fileName, Stream fileStream)
