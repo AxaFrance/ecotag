fff2c6699be6ced20cf14734cb89f3e724ceec07
lilian-delouvy <lilian.delouvy@gmail.com>
2022-06-02 14:55:41 +0200

    test(EditContainer): add tests on container initialization

--- src/Ml.Cli.WebApp/ClientApp/src/Server/Dataset/Edit/Edit.container.spec.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Server/Dataset/Edit/Edit.container.spec.js
@@ -1,9 +1,44 @@
-﻿import { render } from '@testing-library/react';
-import {Edit} from "./Edit.container";
+﻿import {render, waitFor} from '@testing-library/react';
+import {Edit, EditContainer} from "./Edit.container";
 import {BrowserRouter as Router} from "react-router-dom";
 
 describe('Edit.container', () => {
-    
+    describe('init', () => {
+        it('should init correctly', async () => {
+            const fetch = () => {
+                return {
+                    status: 200,
+                    json: () => Promise.resolve({id: "0001", name: "testName", createDate: Date.now(), isLocked: true, type: "image", files: []})
+                };
+            };
+            const {asFragment, getByText} = render(<Router><EditContainer fetch={fetch}/></Router>);
+            await waitFor(() => expect(getByText(/Verrouiller/i)));
+            expect(asFragment()).toMatchSnapshot();
+        });
+        it('should init with forbidden message', async () => {
+            const fetch = () => {
+                return {
+                    status: 403,
+                };
+            };
+            const {asFragment, getByText} = render(<Router><EditContainer fetch={fetch}/></Router>);
+            await waitFor(() => expect(getByText(/Vous n'avez pas le droit d'accéder à cette ressource./i)));
+            expect(asFragment()).toMatchSnapshot();
+        });
+        it('should init with error message', async () => {
+            const fetch = () => {
+                return {
+                    status: 500,
+                };
+            };
+            const {asFragment, getByText} = render(<Router><EditContainer fetch={fetch}/></Router>);
+            await waitFor(() => expect(getByText(/problème/i)));
+            expect(asFragment()).toMatchSnapshot();
+        });
+    });
+});
+
+describe('Edit', () => {
     const state = {
         files: {
             filesSend: [],
@@ -21,15 +56,15 @@ describe('Edit.container', () => {
             isLock: false
         }
     };
-    
+
     const lock = {
         onSubmit: () => {},
         onCancel: () => {},
         onLockDataset: () => {}
     };
-    
+
     it('should render correctly', () => {
         const {asFragment} = render(<Router><Edit state={state} setState={() => {}} fetch={() => {}} lock={lock}/></Router>);
         expect(asFragment()).toMatchSnapshot();
     });
-})
+});
