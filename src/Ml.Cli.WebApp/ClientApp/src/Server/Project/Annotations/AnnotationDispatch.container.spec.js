d34a6e2ff6df90009719700e1bb096130d7022d5
lilian-delouvy <lilian.delouvy@gmail.com>
2022-06-09 15:08:17 +0200

    test(Annotation): check resilience handling

--- src/Ml.Cli.WebApp/ClientApp/src/Server/Project/Annotations/AnnotationDispatch.container.spec.js
+++ src/Ml.Cli.WebApp/ClientApp/src/Server/Project/Annotations/AnnotationDispatch.container.spec.js
@@ -1,40 +1,10 @@
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import {resilienceStatus} from "../../shared/Resilience";
-import {init, initialState} from "./Annotation.hook";
+import {annotate, init, initialState, reserveAnnotation} from "./Annotation.hook";
 import {reducer} from "./Annotation.reducer";
 
-const fetch = () => Promise.resolve({
-  "id": "0001",
-  "name": "Relevé d'information",
-  "datasetId": "0004",
-  "numberTagToDo": 10,
-  "createDate": "04/04/2011",
-  "annotationType": "NER",
-  "text": "Enim ad ex voluptate culpa non cillum eu mollit nulla ex pariatur duis. Commodo officia deserunt elit sint officia consequat elit laboris tempor qui est ex. Laborum magna id deserunt ut fugiat aute nulla in Lorem pariatur. Nostrud elit consectetur exercitation exercitation incididunt consequat occaecat velit voluptate nostrud sunt. Consectetur velit eu amet minim quis sunt in.",
-  "labels": [{"name": "Recto", "color": "#212121", "id": 0}, {"name": "Verso", "color": "#ffbb00", "id": 1}, {"name": "Signature", "color": "#f20713", "id": 2}],
-  "users": [
-    {"annotationCounter": 10,
-      "annotationToBeVerified": 1,
-      "email": "clement.trofleau.lbc@axa.fr"},
-    {"annotationCounter": 24,
-      "annotationToBeVerified": 5,
-      "email": "Guillaume.chervet@axa.fr"},
-    {"annotationCounter": 35,
-      "annotationToBeVerified": 15,
-      "email": "Gille.Cruchont@axa.fr"}
-  ]
-});
-
 describe('AnnotationDispatch.container', () => {
-  const givenUser = {};
-  const givenDataset = {
-    "id": "0001",
-    "name": "Carte verte",
-    "type": "Image",
-    "numberFiles": 300,
-    "createDate": "30/10/2019"
-  };
   const givenProject = {
     "id": "0001",
     "name": "Relevé d'information",
@@ -56,6 +26,11 @@ describe('AnnotationDispatch.container', () => {
         "email": "Gille.Cruchont@axa.fr"}
     ]
   };
+
+  const resilienceTestCases = [
+    [403, resilienceStatus.FORBIDDEN],
+    [500, resilienceStatus.ERROR]
+  ];
 
   function fail(message = "The fail function was called") {
     throw new Error(message);
@@ -126,12 +101,7 @@ describe('AnnotationDispatch.container', () => {
     });
     
     describe('resilience fail', () => {
-      const testCases = [
-        [403, resilienceStatus.FORBIDDEN],
-        [500, resilienceStatus.ERROR]
-      ];
-      
-      test.each(testCases)(
+      test.each(resilienceTestCases)(
           "given %p fetch project response, should return related resilience status",
           async (fetchStatus, expectedStatus) => {
             givenFetch = jest.fn(() => Promise.resolve({status: fetchStatus}));
@@ -145,5 +115,53 @@ describe('AnnotationDispatch.container', () => {
       )
     });
   });
+  
+  describe('.reserveAnnotation()', () => {
+    it('should stop reservation if loading', async () => {
+      let givenDispatch = jest.fn();
+      await reserveAnnotation(() => {}, givenDispatch, {})('0001', '0001', 0, resilienceStatus.LOADING);
+      expect(givenDispatch).toHaveBeenCalledTimes(0);
+    });
+    
+    test.each(resilienceTestCases)(
+        "given %p fetch reserve annotations response status, should dispatch related resilience status",
+        async (fetchStatus, expectedStatus) => {
+          let givenFetch = jest.fn()
+              .mockResolvedValueOnce({status: fetchStatus});
+          let givenDispatch = jest.fn();
+          await reserveAnnotation(givenFetch, givenDispatch, {})('0001', '0001', 0, resilienceStatus.SUCCESS);
+          expect(givenDispatch).toHaveBeenCalledWith({type: 'reserve_annotation', data: {items: [], status: expectedStatus}});
+        }
+    )
+  });
+  
+  describe('.annotate()', () => {
+    const statusTestCases = [
+        ["loading", resilienceStatus.LOADING],
+        ["post", resilienceStatus.POST]
+    ];
+    test.each(statusTestCases)(
+        "given %p starting status, should stop annotation",
+        async (message, statusCase) => {
+          const givenDispatch = jest.fn();
+          await annotate(() => {}, givenDispatch, {})('0001', '0001', {}, '0001', '', statusCase);
+          expect(givenDispatch).toHaveBeenCalledTimes(0);
+        }
+    );
+    
+    test.each(resilienceTestCases)(
+        "given %p fetch annotation response status, should dispatch related resilience status",
+        async (fetchStatus, expectedStatus) => {
+          const givenFetch = jest.fn()
+              .mockResolvedValueOnce({status: fetchStatus});
+          const givenDispatch = jest.fn();
+          const history = {
+            push: jest.fn()
+          };
+          await annotate(givenFetch, givenDispatch, history)('0001', '0001', {}, '0001', '', resilienceStatus.SUCCESS);
+          expect(givenDispatch).toHaveBeenCalledWith({type: 'annotate', data: {status: expectedStatus, annotation: {}, fileId: '0001'}});
+        }
+    )
+  })
 });
 
