6be267c295d61041b174d8cd2f60e185ef8ad231
Lilian Delouvy <lilian.delouvy@axa.fr>
2023-04-12 16:11:33 +0200

    feat(Storybook): add translation and locale button

--- src/Ecotag/ClientApp/.storybook/preview.js
+++ src/Ecotag/ClientApp/.storybook/preview.js
@@ -1,3 +1,7 @@
+import i18n from '../src/i18n';
+import {Suspense, useEffect} from "react";
+import {I18nextProvider} from "react-i18next";
+
 export const parameters = {
     actions: {argTypesRegex: "^on[A-Z].*"},
     controls: {
@@ -6,4 +10,37 @@ export const parameters = {
             date: /Date$/,
         },
     },
-}
+};
+
+export const globalTypes = {
+    locale: {
+        name: 'Locale',
+        description: 'Internationalization locale',
+        toolbar: {
+            icon: 'globe',
+            items: [
+                {value: 'en', title: 'English'},
+                {value: 'fr', title: 'French'}
+            ],
+            showName: true
+        }
+    }
+};
+
+const withI18next = (Story, context) => {
+    const {locale} = context.globals;
+
+    useEffect(() => {
+        i18n.changeLanguage(locale);
+    }, [locale]);
+
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <I18nextProvider i18n={i18n}>
+                <Story/>
+            </I18nextProvider>
+        </Suspense>
+    );
+};
+
+export const decorators = [withI18next];
